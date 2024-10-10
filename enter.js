30218b61d9fc05ddaeb378f3c5631b1cc611446f
Saloni Gite <salonigite2003@gmail.com>
2024-10-10 22:27:59 +0530

    Add files via upload

---
+++ enter.js
@@ -0,0 +1,45 @@
+// const os = require('os')
+// const fs = require('fs')
+// EOL
+// const data = ` Line 1 ${os.EOL} Line 2 ${os.EOL} Line 3`
+// fs.writeFile('yoyo.txt',data,(err)=>{
+//     if (err) throw err 
+//     console.log('done')
+// })
+
+// availableParallelism
+// const threads = os.availableParallelism()
+// const data = `the available threads are ${threads}`
+// fs.writeFile('yoyo.txt' , data , (err)=>{
+//     if (err) throw err
+//     console.log("done")
+// })
+// console.log(`the available threads are ${threads}`);
+
+// OS.ARCH
+// const arch = os.arch()
+// console.log(`the architecture are ${arch}`)
+
+// OS.CPUS
+// const info = os.cpus()
+// const data = `the info about each core is :${info}`
+// fs.writeFile('yoyo.txt',data ,(err)=>{
+//     if (err) throw  err
+//     console.log("done")
+// })
+
+// OS.platform
+// const ose = os.platform()
+// console.log(`the os is ${ose}`)
+
+// os.release
+// const rel = os.release()
+// console.log(`the os is ${rel}`)
+
+// os.totalmem
+// const mem = os.totalmem()
+// console.log(`the memory is ${mem} `)
+
+// os.version
+// const ver = os.version()
+// console.log(`the version is ${ver}`)
